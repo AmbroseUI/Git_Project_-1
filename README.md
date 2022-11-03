018eee1707eccfc703292b440ed97c932f23eaa7
AmbroseUI <emmanueldan@uniuyo.edu.ng>
2022-11-03 21:29:17 +0100

    Added Readme file and setup file

---
+++ README.md
@@ -0,0 +1,14 @@
+#distribution package
+
+
+Summary of the package
+
+
+#Files
+
+
+Expanation of the files in the package
+
+
+#Installation
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribution_Package',
+      version='1.01',
+      description='Gaussian and Binomial distributions',
+      packages=['distribution_Package'],
+      author = 'Emmanuel Ambrose Dan',
+      author_email = 'emmanueldan@uniuyo.edu.ng',
+      zip_safe=False)
